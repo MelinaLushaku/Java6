5c00aa611008cf69569ee4a8816fdc9ee0293280
Melina Lushaku <ml45784@ubt-uni.net>
2020-05-07 14:08:35 +0200

    first Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function shtoLajmin(){
+  var Divv = document.createElement('div');
+  Divv.id = 'block';
+  Divv.className='block';
+  document.getElementsByTagName('body')[0].appendChild(Divv);
+
+}
